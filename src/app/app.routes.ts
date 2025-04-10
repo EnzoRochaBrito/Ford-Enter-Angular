a503cdef1be077db692e0c33757adfc20560ee4a
ENZO ROCHA BRITO <023.933327@senaicimatec.edu.br>
2025-04-10 15:55:05 -0300

    criando as paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LoginComponent
+    },
+    {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path: "dashboard",
+        component: DashboardComponent
+    }
+];
