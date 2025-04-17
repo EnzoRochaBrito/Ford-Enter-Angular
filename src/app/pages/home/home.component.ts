12f4fb89c670df42459a4a6fe1c0a05057ae8043
enzo <enzo1brich@gmail.com>
2025-04-17 20:18:18 -0300

    adaptando o home devido as alterações no card component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
-  imports: [CardComponent, SidebarComponent],
+  imports: [CardComponent, SidebarComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+cardStyleHome = {'width.px': '680', 'height.px': '480'}
 }
